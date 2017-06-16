2ccbd79bb88a608ebc18593c99cf4ac8e6851cb8
Will Roberts <will.roberts11@gmail.com>
2017-06-15 23:16:25 -0700

    100% test coverage for package tmx

--- tmx/tmx_test.go
+++ tmx/tmx_test.go
@@ -4,9 +4,42 @@ import (
 	"testing"
 )
 
-func TestGetMap(t *testing.T) {
-	_, err := getMap("testassets/grass.tmx")
+const (
+	tiledFile   string = "testassets/grass.tmx"
+	badFile     string = "README.md"
+	missingFile string = "missing.file"
+)
+
+func TestGetDimensions(t *testing.T) {
+	d, err := GetDimensions(tiledFile)
 	if err != nil {
+		t.FailNow()
+	}
+
+	// Fixture tests.
+	if d.W != 400 || d.H != 260 || d.TileW != 20 || d.TileH != 20 {
+		t.FailNow()
+	}
+
+	d, err = GetDimensions(badFile)
+	if err == nil {
+		t.FailNow()
+	}
+}
+
+func TestGetMap(t *testing.T) {
+	_, err := getMap(tiledFile)
+	if err != nil {
+		t.FailNow()
+	}
+
+	_, err = getMap(badFile)
+	if err == nil {
+		t.FailNow()
+	}
+
+	_, err = getMap(missingFile)
+	if err == nil {
 		t.FailNow()
 	}
 }
