7ff08beb918158ea6627c0cc6afef903345ffc8f
Will Roberts <will.roberts11@gmail.com>
2017-06-19 00:49:44 -0700

    Converts main.go to scene

---
+++ game/scenes/ninebyfive.go
@@ -0,0 +1,90 @@
+package scenes
+
+import (
+	"github.com/veandco/go-sdl2/sdl"
+	"github.com/willroberts/tactics/engine"
+	"github.com/willroberts/tactics/game/unit"
+	"github.com/willroberts/tactics/grid"
+)
+
+const (
+	gridW int = 9
+	gridH int = 5
+	cellW int = 100
+	cellH int = 100
+	winW  int = gridW * cellW
+	winH  int = gridH * cellH
+
+	fTime   uint32 = 1000 / 30
+	cLtBlue uint32 = 0xff6495ed
+	cDkBlue uint32 = 0xff4682b4
+	cWhite  uint32 = 0xffffffff
+)
+
+type nineByFiveScene struct {
+	grid grid.Grid
+	eng  engine.SDLEngine
+}
+
+func (s *nineByFiveScene) Setup() error {
+	s.grid = grid.NewGrid(gridW, gridH, cellW, cellH)
+	s.grid.Checkerboard(cLtBlue, cDkBlue)
+
+	s.grid.Cell(0, 2).SetContents(unit.NewUnit())
+	s.grid.Cell(4, 0).SetContents(unit.NewUnit())
+	s.grid.Cell(4, 4).SetContents(unit.NewUnit())
+	s.grid.Cell(5, 2).SetContents(unit.NewUnit())
+	s.grid.Cell(8, 2).SetContents(unit.NewUnit())
+
+	return nil
+}
+
+func (s *nineByFiveScene) Main() error {
+	err := s.eng.ClearScreen()
+	if err != nil {
+		return err
+	}
+
+	for _, col := range s.grid.Cells() {
+		for _, cell := range col {
+			d := cell.Dimensions()
+			err = s.eng.DrawRect(&sdl.Rect{
+				X: int32(d.X),
+				Y: int32(d.Y),
+				W: int32(d.W),
+				H: int32(d.H),
+			}, cell.Color())
+			if err != nil {
+				return err
+			}
+
+			if cell.IsOccupied() {
+				err = s.eng.DrawRect(&sdl.Rect{
+					X: int32(d.X + (d.W / 4)),
+					Y: int32(d.Y + (d.H / 4)),
+					W: int32(d.W / 2),
+					H: int32(d.H / 2),
+				}, cWhite)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	err = s.eng.UpdateSurface()
+	if err != nil {
+		return err
+	}
+
+	s.eng.PauseRendering(fTime)
+	return nil
+}
+
+func (s *nineByFiveScene) Teardown() error {
+	return nil
+}
+
+func NewNineByFiveScene(e engine.SDLEngine) engine.Scene {
+	return &nineByFiveScene{eng: e}
+}
--- main.go
+++ main.go
@@ -3,92 +3,32 @@ package main
 import (
 	"log"
 
-	"github.com/veandco/go-sdl2/sdl"
 	"github.com/willroberts/tactics/engine"
-	"github.com/willroberts/tactics/game/unit"
-	"github.com/willroberts/tactics/grid"
+	"github.com/willroberts/tactics/game/scenes"
 )
 
 const (
-	gridWidth  int = 9
-	gridHeight int = 5
-
-	// Using multiples of four so we can cut a cell into quadrants easily.
-	cellWidth    int = 100
-	cellHeight   int = 100
-	windowWidth  int = gridWidth * cellWidth
-	windowHeight int = gridHeight * cellHeight
-
-	framesPerSecond uint32 = 30
-	frameTime       uint32 = 1000 / framesPerSecond // Milliseconds.
-
-	colorRed    uint32 = 0xffff0000
-	colorGreen  uint32 = 0xff00ff00
-	colorLtBlue uint32 = 0xff6495ed
-	colorDkBlue uint32 = 0xff4682b4
-	colorWhite  uint32 = 0xffffffff
+	winW int = 900
+	winH int = 500
 )
 
 func main() {
-	// FIXME: Move some things to init(), leaving only the loop in main().
-	g := grid.NewGrid(gridWidth, gridHeight, cellWidth, cellHeight)
-
-	// TODO: Load textures from a TMX file.
-	g.Checkerboard(colorLtBlue, colorDkBlue)
-
-	// Adds units.
-	// Positions chosen for testing. Replace with something legitimate.
-	g.Cell(0, 2).SetContents(unit.NewUnit())
-	g.Cell(8, 2).SetContents(unit.NewUnit())
-	g.Cell(4, 0).SetContents(unit.NewUnit())
-	g.Cell(4, 4).SetContents(unit.NewUnit())
-	g.Cell(5, 2).SetContents(unit.NewUnit())
-
-	eng, err := engine.NewSDLEngine("tactics", windowWidth, windowHeight)
+	eng, err := engine.NewSDLEngine("tactics", winW, winH)
 	if err != nil {
 		log.Fatalln("failed to initalize sdl engine:", err)
 	}
 	defer eng.Window().Destroy()
 
+	scene := scenes.NewNineByFiveScene(eng)
+	if err = scene.Setup(); err != nil {
+		log.Fatalln("error during scene setup:", err)
+	}
+
 	for {
-		if err = eng.ClearScreen(); err != nil {
-			log.Fatalln("error clearing screen:", err)
-		}
+		scene.Main()
+	}
 
-		// FIXME: Use a channel for iteration.
-		for _, col := range g.Cells() {
-			for _, cell := range col {
-				// Draw cells.
-				d := cell.Dimensions()
-				rect := &sdl.Rect{
-					X: int32(d.X),
-					Y: int32(d.Y),
-					W: int32(d.W),
-					H: int32(d.H),
-				}
-				if err = eng.DrawRect(rect, cell.Color()); err != nil {
-					log.Fatalln("error drawing cell:", err)
-				}
-
-				// Draw units and objects.
-				if cell.IsOccupied() {
-					unitRect := &sdl.Rect{
-						X: int32(d.X + (d.W / 4)),
-						Y: int32(d.Y + (d.H / 4)),
-						W: int32(d.W / 2),
-						H: int32(d.H / 2),
-					}
-					if err = eng.DrawRect(unitRect, colorWhite); err != nil {
-						log.Fatalln("error drawing unit:", err)
-					}
-				}
-			}
-		}
-
-		if err = eng.UpdateSurface(); err != nil {
-			log.Fatalln("error updating surface:", err)
-		}
-
-		eng.PauseRendering(frameTime)
+	if err = scene.Teardown(); err != nil {
+		log.Println("error during scene teardown:", err)
 	}
 }
